564f77ece867700a613293d2b9d4eeb726308ea4
Samuel Hunter <swilkinsonhunter@gmail.com>
2014-11-27 22:41:16 -0600

    commit for merge

--- code/DocumentParser.cpp
+++ code/DocumentParser.cpp
@@ -46,7 +46,6 @@ void DocumentParser::parseDrive(string xmlInFile, IndexHandler*& indexhandler)
   int looper = 0; //used to only get a certain amount of xml file
   string inString;
 
-
   while(!inXMLstream.eof() /*&& looper < 200*/)
   {
     //read in next word
@@ -162,6 +161,7 @@ void DocumentParser::parseDrive(string xmlInFile, IndexHandler*& indexhandler)
           username = "none";
 
         page->setContributingUser(username);
+        //cout << "did this make sure?" << endl;
 
         cout << looper++ << "\t" << title << " by " << username << " has id " << id << endl;
 
